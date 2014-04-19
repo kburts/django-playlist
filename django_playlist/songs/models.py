ee8d73057c3b6ddd23ff686770d3529522c99f62
kburts <kevin.burton2@gmail.com>
2014-04-18 22:18:48 -0700

    Added playlist and song models.

--- django_playlist/songs/models.py
+++ django_playlist/songs/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils import timezone
 
+from auth.models import User
 # Create your models here.
+
+class Playlist(models.Model):
+    title = models.CharField(max_length=200)
+    creation_date = models.DateTimeField('date created', default=timezone.now())
+    user = models.FoerignKey(User)
+
+    def __unicode__(self):
+        return self.title
+
+class Song(models.Model):
+    name = models.CharField(max_length=200)
+    artist = models.CharField(max_length=200)
+    playlist = models.ForeignKey(Playlist)
+
+    def __unicode__(self):
+        return (self.name + " - " + self.artist)
